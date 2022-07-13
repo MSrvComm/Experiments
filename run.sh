1f4971b668a40594163cd6aaadf5512ad89d1b81
Ratnadeep Bhattacharya <ratnadeep.bhattacharya1983@gmail.com>
2022-07-12 23:08:46 -0500

    bugfix

--- run.sh
+++ run.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-
-# Build the docker images
-./build.sh
-
 # Set up the cluster
-./edit_cluster.sh
+. edit_cluster.sh
 
 sleep 60
 
 # Run the experiment
 for EXP in "lc1" "lc40" "bloc40"
 do
+    echo "deploying $EXP"
     . deploy.sh $EXP
     sleep 120
-    hey -c 100 -z 5m -disable-keepalive -o csv http://localhost:$PORT_NUM/svc/0 | tee ($EXP).csv
+    hey -c 100 -z 5m -disable-keepalive -o csv http://localhost:$PORT_NUM/svc/0 | tee $EXP.csv
     sleep 20
     ./delete.sh $EXP
     sleep 120
 done
 
 # Graph the data
+sudo apt install python3-pip
+pip install -r requirements.txt
 ./graph.py
