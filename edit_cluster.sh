7241baab8bde345b7405ca0b83e21cbbe5739aac
Ratnadeep Bhattacharya <ratnadeepb@users.noreply.github.com>
2022-10-20 11:33:19 -0400

    Update edit_cluster.sh
    correct!

--- edit_cluster.sh
+++ edit_cluster.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd BLOCControl/restEpWatcher && . deploy.sh && cd $OLDPWD
 kubectl taint nodes --all node-role.kubernetes.io/master-
-kubectl label nodes $(kubectl get nodes | awk 'NR>1 {print $1} NR==4{exit}' | tr '\n' ' ') workload=svc1
+kubectl label nodes $(kubectl get nodes | awk 'NR>1 {print $1} NR==4{exit}' | tr '\n' ' ') workload=svc2
 kubectl label node $(kubectl get nodes | awk 'NR==5 {print $1}') workload=svc1
